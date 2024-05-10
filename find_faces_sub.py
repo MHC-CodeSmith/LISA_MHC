3af63994fa1feae21957ccb380bed7710261112c
Matheus Hipólito Carvalho <102363489+MHC-CodeSmith@users.noreply.github.com>
2024-05-10 15:44:58 -0300

    Update find_faces_sub.py

--- find_faces_sub.py
+++ find_faces_sub.py
@@ -18,7 +18,7 @@ def callback(data):
     br = CvBridge()
  
     # Output debugging information to the terminal
-    rospy.loginfo("Recebendo Rosto frame")
+    rospy.loginfo("Recebendo Rostos")
    
     # Convert ROS Image message to OpenCV image
     current_frame = br.imgmsg_to_cv2(data)
@@ -34,7 +34,7 @@ def receive_message():
     rospy.init_node('faces_sub_py', anonymous=True)
    
     # Subscribe to the /Face topic
-    rospy.Subscriber('/Face', Image, callback)
+    rospy.Subscriber('/Rostos', Image, callback)
  
     # Keep the node running
     rospy.spin()
