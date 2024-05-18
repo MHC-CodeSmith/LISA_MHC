7a5296b2db6fd7c655d4a4dac45df3af0e747fba
Matheus Hipólito Carvalho <102363489+MHC-CodeSmith@users.noreply.github.com>
2024-05-17 21:03:46 -0300

    Update serial_node.py

--- serial_node.py
+++ serial_node.py
@@ -1,55 +1,49 @@
 #!/usr/bin/python3
 # Description:
 # - Subscribes to real-time streaming video from your built-in webcam.
+# - Sends commands to a serial device based on the received video stream data.
 #
 # Author:
 # - Addison Sears-Collins
 # - https://automaticaddison.com
  
 # Import the necessary libraries
-import rospy # Python library for ROS
-from sensor_msgs.msg import 
+import rospy
+from sensor_msgs.msg import Image
 import serial
 import time
-
-
-
+# If you need to convert ROS Image messages to OpenCV format uncomment the next line
+# from cv_bridge import CvBridge
+ 
 def callback(data):
-  #só até 4 contador
-  #1: parar 2: horario 3: antihorario
-  # Used to convert between ROS and OpenCV images
- 
-  # Output debugging information to the terminal
-  rospy.loginfo("Recebendo contagem de dedos")
+  rospy.loginfo("Receiving finger count")
+  contador = int(data.data)  # Assuming data contains the finger count as integer
+
+  # Here, add your conditions based on the value of 'contador'
+  if contador == 1:
+      command = 'PARAR'
+  elif contador == 2:
+      command = 'HORARIO'
+  elif contador == 3:
+      command = 'ANTIHORARIO'
+  else:
+      command = 'NOP'  # No operation
+
+  # Send command to the serial port
+  ser.write(command.encode('utf-8') + b'\n')
+  rospy.loginfo(f"Sent command: {command}")
    
-  contador = int(data)
-   
-      
 def receive_message():
- 
-  # Tells rospy the name of the node.
-  # Node name is set to Receber_Imagem
   rospy.init_node('Node_Serial', anonymous=False)
-   
-  # Node is subscribing to the /Imagens topic
   rospy.Subscriber('finger_count', Image, callback)
- 
-  # spin() simply keeps python from exiting until this node is stopped
   rospy.spin()
- 
-  # Close down the video stream when done
-  cv2.destroyAllWindows()
+  # cv2.destroyAllWindows()  # Uncomment if using OpenCV to handle video windows
   
 if __name__ == '__main__':
-  # Substitua '/dev/ttyUSB0' pelo dispositivo correto se for diferente
-  ser = serial.Serial('/dev/ttyUSB0', 115200)  # Abre a porta serial com 115200 baud
-  
+  ser = serial.Serial('/dev/ttyUSB0', 115200)  # Open the serial port at 115200 baud
   try:
-      #while True:
-          #user_command = input("Controle do motor (HORARIO, ANTIHORARIO, PARAR): \n")
-          #ser.write(user_command.encode('utf-8') + b'\n')  # Adiciona nova linha e codifica para bytes
-      ser.write(msg)    
-      time.sleep(1)  # Espera um segundo antes de enviar a próxima mensagem
+      receive_message()
+  except rospy.ROSInterruptException:
+      pass  # Handle ROS interrupt exception gracefully
   except KeyboardInterrupt:
-      ser.close()  # Fecha a porta serial ao sair do programa
-  
+      ser.close()  # Close the serial port when exiting the program
