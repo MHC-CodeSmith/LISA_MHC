cd413ed326465bfcdc586f828e4f0931c55e292d
Matheus Hipólito Carvalho <102363489+MHC-CodeSmith@users.noreply.github.com>
2024-05-24 16:21:48 -0300

    Update gestos_pub-sub.py
    versão corrigida gestos

--- gestos_pub-sub.py
+++ gestos_pub-sub.py
@@ -5,44 +5,69 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 from std_msgs.msg import Int32
 
+# Baixar o modelo de reconhecimento de gestos (execute uma vez antes de rodar o script)
+# !wget -q https://storage.googleapis.com/mediapipe-models/gesture_recognizer/gesture_recognizer/float16/1/gesture_recognizer.task
+
+# Configurações do modelo
+base_options = python.BaseOptions(model_asset_path='gesture_recognizer.task')
+options = vision.GestureRecognizerOptions(base_options=base_options)
+recognizer = vision.GestureRecognizer.create_from_options(options)
+
+# Inicializa o CvBridge uma vez
+bridge = CvBridge()
+
+# Mapeamento de gestos para inteiros
+gesture_mapping = {
+    'Thumbs_Up': 1,
+    'Thumbs_Down': 2,
+    'Open_Palm': 3,
+    'Closed_Fist': 4,
+    'Victory': 5,
+    'Pointing_Up': 6,
+}
+
 def detect_hand_gesture(frame):
-    mp_hands = mp.solutions.hands
-    hands = mp_hands.Hands(
-        static_image_mode=False,
-        max_num_hands=2,
-        min_detection_confidence=0.5,
-        min_tracking_confidence=0.5)
-    mp_drawing = mp.solutions.drawing_utils
-
+    # Converte a imagem de BGR para RGB
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
 
+    # Reconhece gestos na imagem
+    recognition_result = recognizer.recognize(mp_image)
     gesture = None
-    if results.multi_hand_landmarks:
-        # Your gesture recognition logic here
-        # For example, if a specific hand landmark is detected, set gesture to 1
-        gesture = 1
+
+    if recognition_result.gestures:
+        # Obtém o gesto principal reconhecido
+        top_gesture = recognition_result.gestures[0][0]
+        gesture = top_gesture.category_name
+        rospy.loginfo("Gesto detectado: {}".format(gesture))
+    else:
+        rospy.loginfo("Nenhum gesto detectado.")
 
     return gesture
 
 def image_callback(msg):
-    bridge = CvBridge()
-    frame = bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
+    global gesture_pub
+    frame = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
     gesture = detect_hand_gesture(frame)
-    if gesture is not None:
+    if gesture in gesture_mapping:
         rospy.loginfo("Gesto reconhecido: {}".format(gesture))
-        gesture_pub.publish(gesture)
+        gesture_pub.publish(Int32(data=gesture_mapping[gesture]))
+    else:
+        rospy.loginfo("Gesto não reconhecido ou não mapeado: {}".format(gesture))
 
 def gesture_recognition():
-    rospy.init_node('reconhecimento_gesto_nodo', anonymous=True)
-    rospy.Subscriber('/Imagens', Image, image_callback)
     global gesture_pub
+    rospy.init_node('reconhecimento_gesto_nodo', anonymous=True)
     gesture_pub = rospy.Publisher('/Gestos', Int32, queue_size=10)
+    rospy.Subscriber('/Imagens', Image, image_callback)
+    rospy.loginfo("Nó de reconhecimento de gestos iniciado.")
     rospy.spin()
 
-if __name__ == '_main_':
+if _name_ == '_main_':
     try:
         gesture_recognition()
     except rospy.ROSInterruptException:
