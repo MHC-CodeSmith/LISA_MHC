bedbe7c3d767136154ba1a57c86606076074b68f
Matheus Hipólito Carvalho <102363489+MHC-CodeSmith@users.noreply.github.com>
2024-06-27 14:12:18 -0300

    Update cam_pub.py

--- scripts/cam_pub.py
+++ scripts/cam_pub.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-# Basics ROS program to publish real-time streaming 
-# video from your built-in webcam
-# Author:
-# - Addison Sears-Collins
-# - https://automaticaddison.com
- 
+
 # Import the necessary libraries
 import rospy # Python library for ROS
 from sensor_msgs.msg import Image # Image is the message type
@@ -18,14 +13,12 @@ def publish_message():
   pub = rospy.Publisher('/Imagens', Image, queue_size=10)
      
   # Tells rospy the name of the node.
-  # Node name is set to Receber_Imagem
   rospy.init_node('Receber_Imagem', anonymous=False)
      
   # Go through the loop 10 times per second
   rate = rospy.Rate(10) # 10hz
      
   # Create a VideoCapture object
-  # The argument '0' gets the default webcam.
   cap = cv2.VideoCapture(0)
      
   # Used to convert between ROS and OpenCV images
@@ -33,24 +26,15 @@ def publish_message():
    # Desired resolution
   desired_width = 320
   desired_height = 240
-  # While ROS is still running.
   while not rospy.is_shutdown():
      
       # Capture frame-by-frame
-      # This method returns True/False as well
-      # as the video frame.
       ret, frame = cap.read()
          
       if ret == True:
-        # Print debugging information to the terminal
-        #rospy.loginfo('Publicando video frame')
         resized_frame = cv2.resize(frame, (desired_width, desired_height))
              
         # Publish the image.
-        # The 'cv2_to_imgmsg' method converts an OpenCV
-        # image to a ROS image message
-        # cv2.imshow("Imagem Original", frame)
-        # cv2.waitKey(1)
         pub.publish(br.cv2_to_imgmsg(frame))
              
       # Sleep just enough to maintain the desired rate
