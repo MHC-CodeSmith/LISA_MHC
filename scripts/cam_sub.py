86386013fa9fdca5125b4eb12391c0690760ce15
Matheus Hipólito Carvalho <102363489+MHC-CodeSmith@users.noreply.github.com>
2024-06-27 14:14:04 -0300

    Update cam_sub.py

--- scripts/cam_sub.py
+++ scripts/cam_sub.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
-# Description:
-# - Subscribes to real-time streaming video from your built-in webcam.
-#
-# Author:
-# - Addison Sears-Collins
-# - https://automaticaddison.com
- 
-# Import the necessary libraries
-import rospy # Python library for ROS
-from sensor_msgs.msg import Image # Image is the message type
-from cv_bridge import CvBridge # Package to convert between ROS and OpenCV Images
-import cv2 # OpenCV library
+
+ import rospy 
+from sensor_msgs.msg import Image 
+from cv_bridge import CvBridge 
+import cv2 
  
 def callback(data):
  
@@ -31,10 +24,8 @@ def callback(data):
 def receive_message():
  
   # Tells rospy the name of the node.
-  # Node name is set to Receber_Imagem
   rospy.init_node('Mostrar_Imagem', anonymous=False)
    
-  # Node is subscribing to the /Imagens topic
   rospy.Subscriber('/Imagens', Image, callback)
  
   # spin() simply keeps python from exiting until this node is stopped
